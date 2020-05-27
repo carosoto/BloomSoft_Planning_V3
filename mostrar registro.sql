3d4f51a6bcf09d4411cca40dc9cb27466497912a
Carolina Soto <kroo41095@gmail.com>
2020-05-26 19:05:31 -0500

    cambio tarjeta

---
+++ mostrar registro.sql
@@ -0,0 +1,10 @@
+﻿/*Delete from TarjetaRequerim*/
+
+/*INSERT INTO TarjetaRequerim(id_requerimiento,id_partidaJugador,nivel_tax,tiempo,dificultad,puntos)
+VALUES ('5','303000001','2','2','2','2');*/
+
+/*INSERT INTO Tarea(id_tarjetaRequerim,descripcion)
+VALUES ('30','prueba requerimientos');*/
+
+INSERT INTO VerboTax(nivel_tax,verbos)
+VALUES ('2','prueba verbos');
