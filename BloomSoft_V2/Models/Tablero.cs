dc97f84fbe733aa1fa5863f621fe190666224dd6
Carolina Soto <kroo41095@gmail.com>
2020-06-09 02:45:18 -0500

    tarjeta tablero

---
+++ BloomSoft_V2/Models/Tablero.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Spatial;
+
+namespace BloomSoft_V2.Models
+{
+    public class Tablero
+    {
+        public IEnumerable<PartidaJugador> JugadorModel { get; set; }
+        //public IEnumerable<Edicion> edicionModel { get; set; }
+
+        public PartidaJugador JugadorM { get; set; }
+        public Edicion edicionM { get; set; }
+
+        public Tablero()
+        {
+            edicionM = new Edicion();
+            JugadorM = new PartidaJugador();
+        }
+    }
+}
